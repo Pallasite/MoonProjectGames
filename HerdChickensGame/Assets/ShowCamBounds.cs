d4ce6bb06ce08e38a83b99185f59be43fae66dcd
Bryce <Pallasite@users.noreply.github.com>
2019-07-30 17:39:55 -0500

    Animated Clouds and SMAA
    Animated all cloud layers for a parallax effect using animation clips and timeline scaling. I'm not sure I used the animation controllers or timelines correctly. I don't think I needed one for each, but it works. I had to creat a parent null object to be able to move the cloud translation during game. If I animated the cloud parent directly the translation became 0,0,0 on run despite being set to other values beforehand.
    
    Changed main camera anti-aliasing to SMAA from TXAA for less artifacting.

---
+++ HerdChickensGame/Assets/ShowCamBounds.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCamBounds : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+        float verticalHeightSeen = Camera.main.orthographicSize * 2.0f;
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, new Vector3((verticalHeightSeen * Camera.main.aspect), verticalHeightSeen, 0));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        void OnDrawGizmos()
+        {
+            float verticalHeightSeen = Camera.main.orthographicSize * 2.0f;
+            float verticalWidthSeen = verticalHeightSeen * Camera.main.aspect;
+
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireCube(transform.position, new Vector3(verticalWidthSeen, verticalHeightSeen, 0));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnDrawGizmos()
+        {
+            float verticalHeightSeen = Camera.main.orthographicSize * 2.0f;
+            float verticalWidthSeen = verticalHeightSeen * Camera.main.aspect;
+
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireCube(transform.position, new Vector3(verticalWidthSeen, verticalHeightSeen, 0));
+        }
+
+    }
+}
